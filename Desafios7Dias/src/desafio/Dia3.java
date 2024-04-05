7f430e3e2522cf65482cd5869d12b36ca3f6d746
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2024-04-05 20:00:12 -0300

    desafio dia 3
    3. Write a Java program to print the following grid.
    
    Expected Output :
    
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -
    - - - - - - - - - -

---
+++ Desafios7Dias/src/desafio/Dia3.java
@@ -0,0 +1,20 @@
+package desafio;
+
+public class Dia3 {
+
+	public static void main(String[] args) {
+		
+	  int[][] vetor = new int[10][10];    
+        
+      for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+               
+                System.out.printf("%2d ", vetor[i][j]);
+            }
+           
+            System.out.println();
+        }
+    }
+}
+
+
