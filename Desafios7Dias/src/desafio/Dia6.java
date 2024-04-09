d1148ae97161297dc1fe42e34c8f09a5a6a371a8
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2024-04-08 21:00:37 -0300

    desafio dia 6
    7. Write a Java program to remove a specific element from an array.

---
+++ Desafios7Dias/src/desafio/Dia6.java
@@ -0,0 +1,56 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class Dia6 {
+	
+	public static void delete(int[] vetor, int valor) {
+	
+	for (int i = 0; i < vetor.length; i++) {
+		if(vetor[i] == valor) {
+			vetor[i] = 0;
+		}
+	
+	}
+
+}
+
+
+	public static void main(String[] args) {
+		Scanner leitura = new Scanner(System.in);
+		
+		String v;
+		System.out.println("Insira o tamanho do vetor deseado : ");
+		String resp = leitura.nextLine();
+		int tamanho = Integer.valueOf(resp);
+		
+		int[] vetor = new int[tamanho];
+		
+		System.out.println("insira " + tamanho + " valores inteiros diferentes de 0 no vetor : ");
+		for (int i = 0; i < vetor.length; i++) {
+			
+			v = leitura.nextLine();
+		    vetor[i] = Integer.valueOf(v);
+			
+		}
+		
+		System.out.println("Insira o valor que deseja deletar : ");
+		String r = leitura.nextLine();
+		int valor = Integer.valueOf(r);
+		
+		
+		delete(vetor, valor);
+		
+			System.out.println("vetor final!!");
+			
+			 for (int i = 0; i < 10; i++) {
+		               
+		                System.out.println(vetor[i]);
+		            }
+		           
+		            System.out.println();
+		        }
+		
+	}
+
+
